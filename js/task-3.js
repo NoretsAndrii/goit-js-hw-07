fccd8bd4f084901b3c458ad5e3fce58181c748ea
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-01-25 01:20:36 +0200

    Create task-3.js

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", (event) => {
+  let textInput = event.currentTarget.value.trim();
+  if (textInput === "" || textInput === " ") {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = textInput;
+  }
+});
