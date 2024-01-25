e2265e27dc49767a9c80a5713e58bdf02efcb756
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-01-25 18:59:03 +0200

    Create task-5.js

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+changeColorBtn.addEventListener("click", (event) => {
+  let newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  span.textContent = newColor;
+});
